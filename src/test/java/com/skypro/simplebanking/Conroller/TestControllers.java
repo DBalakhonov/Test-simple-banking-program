48842dde26df99dc005a5ba997cb3cc1beee3305
Danil Balakhonov <danilbalakhonoff@yandex.ru>
2023-08-07 17:54:33 +0500

    Update TestControllers.java

--- src/test/java/com/skypro/simplebanking/Conroller/TestControllers.java
+++ src/test/java/com/skypro/simplebanking/Conroller/TestControllers.java
@@ -25,6 +25,11 @@ import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -66,8 +71,19 @@ public class TestControllers {
                 .andExpect(status().isOk());
     }
     @Test
+    vois createUserTestWitchTocken() throws Exception{
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("username","test_name");
+        jsonObject.put("password","7852");
+        mockMvc.perform(post("/user")
+            .header("Authorization", "Bearer admin")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(jsonObject.toString())) 
+            .andExpect(status().isOk());
+    } 
+    @Test
     @WithMockUser("user") 
-    void createUserTest() throws Exception {
+    void createUserTestNegative() throws Exception {
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("username","test_name");
         jsonObject.put("password","7852");
@@ -101,24 +117,70 @@ public class TestControllers {
                 .andExpect(status().isOk());
     }
     @Test
+    @WithMockUser("user")
+    void transferTestNegative()throws Exception{
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("fromAccountId",1);
+        jsonObject.put("toUserId",3);
+        jsonObject.put("toAccountId",55);
+        jsonObject.put("amount",1);
+        mockMvc.perform(post("/transfer")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonObject.toString()))
+                .andExpect(status().isForBidden());
+    }
+    @Test
+    @WithMockUser("test_name")
+    void getAccountTest() throws Exception{
+        mockMvc.prform(get("/account/1"))
+            .andExpect(status().isOk()) 
+            } 
+    @Test
+    @WithMockUser("user")
+    void getAccountTest() throws Exception{
+        mockMvc.prform(get("/account/1"))
+            .andExpect(status().isOk()) 
+            } 
+                     
+    @Test
     @WithMockUser(authorities = {"test_name"})
     void depositTest() throws Exception{
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("amount",5);
-        mockMvc.perform(post("/account/deposit/2")
+        mockMvc.perform(post("/account/deposit/1")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(jsonObject.toString()))
                 .andExpect(status().isOk());
+    }
+    @Test
+    @WithMockUser(authorities = {"user"})
+    void depositTestNegative() throws Exception{
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("amount",5);
+        mockMvc.perform(post("/account/deposit/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonObject.toString()))
+                .andExpect(status().isForBidden());
     }
     @Test
     @WithMockUser(authorities = {"test_name"})
     void withdrawTest() throws Exception{
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("amount",1);
-        mockMvc.perform(post("/account/withdraw/2")
+        mockMvc.perform(post("/account/withdraw/1")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(jsonObject.toString()))
                 .andExpect(status().isOk());
+    }
+    @Test
+    @WithMockUser(authorities = {"user"})
+    void withdrawTestNegative() throws Exception{
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("amount",1);
+        mockMvc.perform(post("/account/withdraw/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonObject.toString()))
+                .andExpect(status().isForBidden());
     }
     
 }
